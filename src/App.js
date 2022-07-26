0d80cb0f1076f0f439fcaddad47de25c92e49bb4
mahyarjbr <mahyarjbr1374@gmail.com>
2022-07-26 14:42:06 +0430

    adding icons and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './containers/header/Header';
 import Main from './containers/main/Main';
+import { faStar ,faFile,faBookmark,faSearch,faUserGroup,faUserCheck } from '@fortawesome/free-solid-svg-icons'
 import {
     BrowserRouter as Router,
    
@@ -11,31 +12,37 @@ const App = () => {
         {
           path: "/",
           exact: true,
-          sidebar: () => <div>home!</div>,
-          main: () => <h2>جستجو</h2>
+          sidebar: "جستجو",
+          main: () => <h2>جستجو</h2>,
+          icon:faSearch
         },
         {
           path: "/ticket",
           sidebar: "آگهی های من",
-          main: () => <h2>صفحه مربوط به آگهی های من</h2>
+          main: () => <h2>صفحه مربوط به آگهی های من</h2>,
+          icon:faFile
         },
         {
           path: "/agents",
           sidebar: "نماینده ها",
-          main: () => <h2>صفحه مربوط به نماینده ها</h2>
+          main: () => <h2>صفحه مربوط به نماینده ها</h2>,
+          icon:faUserGroup
         },
         {
             path: "/profile",
             sidebar: "پروفایل",
-            main: () => <h2>صفحه مربوط به پروفایل </h2>
+            main: () => <h2>صفحه مربوط به پروفایل </h2>,
+            icon:faUserCheck
           }, {
             path: "/saved-house",
             sidebar: "ملک های ذخیره شده",
-            main: () => <h2>صفحه مربوط به ملک ها ذخیره شده</h2>
+            main: () => <h2>صفحه مربوط به ملک ها ذخیره شده</h2>,
+            icon:faBookmark
           }, {
             path: "/recent-search",
             sidebar: "جستجو های اخیر",
-            main: () => <h2>صفحه مربوط به جستجو های اخیر</h2>
+            main: () => <h2>صفحه مربوط به جستجو های اخیر</h2>,
+            icon:faSearch
           }]
    
     return (
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,14 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar ,faSearch,faUserGroup,faUserCheck } from '@fortawesome/free-solid-svg-icons'
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
 
 const Header = ({routes}) => {
   return (
-    <div>
-      <ul style={{ listStyleType: "none", padding: 0 }}>
+    <div className="header">
+      <div className="header_top">
+        <img src="" alt="" />
+      </div>
+      <h2 className="header_title">املاک هاشمی نسب</h2>
+      <div className="header_rates">
+      <FontAwesomeIcon icon={faStar}/>
+      </div>
+      <ul className="header_items">
         {routes.map((route ,index) => (
-          <li key={index}>
-            <Link to={route.path}>{route.sidebar}</Link>
+          <li className="header_item" key={index}>
+            
+            <Link to={route.path}>  <FontAwesomeIcon icon={route.icon}/> {route.sidebar}</Link>
           </li>
         ))}
       </ul>
